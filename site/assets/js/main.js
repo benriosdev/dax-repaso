7e56212c21cd7fe3821231797fd29c844ed0bf14
Benja <234678810+benriosdev@users.noreply.github.com>
2026-09-26 19:27:20 +0200

    Add: SEO etiquetas (#2)
    SEO etiquetas para mejorar visibilidad. y Open Graph

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -395,7 +395,21 @@
     });
   }
 
+  function initAnalytics() {
+    if (location.hostname !== "benrios.dev") return;
+    var id = "G-B7JYL0JMEV";
+    var s = document.createElement("script");
+    s.async = true;
+    s.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
+    document.head.appendChild(s);
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function () { window.dataLayer.push(arguments); };
+    window.gtag("js", new Date());
+    window.gtag("config", id);
+  }
+
   document.addEventListener("DOMContentLoaded", function () {
+    initAnalytics();
     renderSidebars();
     renderBreadcrumb();
     wirePdfLink();
